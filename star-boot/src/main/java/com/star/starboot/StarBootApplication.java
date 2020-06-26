4b6bf607db87ad5389b934276edf9697bae9dbf2
xupingyong <865934097@qq.com>
2020-06-26 16:47:20 +0800

    Init

---
+++ star-boot/src/main/java/com/star/starboot/StarBootApplication.java
@@ -0,0 +1,13 @@
+package com.star.starboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarBootApplication.class, args);
+    }
+
+}
---
+++ star-boot/src/test/java/com/star/starboot/StarBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.starboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
