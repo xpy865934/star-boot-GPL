caccd3b3a73681e1c2cc89e5144d2f81ef912fdd
xupingyong <865934097@qq.com>
2020-07-07 11:36:38 +0800

    前端js修改

---
+++ src/main/java/com/star/starboot/system/dao/UsersReRolesMapper.java
@@ -0,0 +1,16 @@
+package com.star.starboot.system.dao;
+
+import com.star.starboot.system.entity.UsersReRoles;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户和角色关联关系 Mapper 接口
+ * </p>
+ *
+ * @author xpy
+ * @since 2020-07-05
+ */
+public interface UsersReRolesMapper extends BaseMapper<UsersReRoles> {
+
+}
--- star-ui/src/settings.js
+++ star-ui/src/settings.js
@@ -1,12 +1,11 @@
 module.exports = {
-
-  title: 'Vue Admin Template',
+  title: '重症质控上报中心',
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: false,
+  fixedHeader: true,
 
   /**
    * @type {boolean} true | false
