dda275834e84d6ecb3d0c9ec0ad2bbb23e4acdcb
xupingyong <865934097@qq.com>
2020-07-24 10:23:41 +0800

    注释

--- star-ui/src/permission.js
+++ star-ui/src/permission.js
@@ -4,8 +4,8 @@ import store from './store'
 import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
-import { generateTitle } from '@/utils/i18n'
-import getPageTitle from '@/utils/get-page-title'
+// import { generateTitle } from '@/utils/i18n'
+// import getPageTitle from '@/utils/get-page-title'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
