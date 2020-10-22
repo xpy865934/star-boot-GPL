c2951552929d303e69b3746be1c4ef6f639c7263
xpy <865934097@qq.com>
2020-10-22 14:47:37 +0800

    用户查询所有用户

--- star-ui/src/api/app.js
+++ star-ui/src/api/app.js
@@ -6,3 +6,10 @@ import request from '@/utils/request'
 export function getFirstDictAll(params) {
   return request.post('/firstDict/getFirstDictAll', params, false)
 }
+
+/**
+ * 获取所有的用户信息
+ */
+export function getUserList(params) {
+  return request.post('/users/queryList', params, false)
+}
