5030b647017d47dd7e9ce241138a5823fc1264a7
xpy <865934097@qq.com>
2020-11-26 16:46:01 +0800

    查看样式以及翻译用户

--- star-ui/src/main.js
+++ star-ui/src/main.js
@@ -20,7 +20,7 @@ import '@/permission' // permission control
 
 import config from '@/config' // config
 
-import { isEmpty, isIntNum, isNumber, decimalPercentConvert, formatPercent, formatThousandth, translateFirstDict } from '@/utils/common'
+import { isEmpty, isIntNum, isNumber, decimalPercentConvert, formatPercent, formatThousandth, translateFirstDict, translateUserName } from '@/utils/common'
 
 // 自定义组件
 // 分页   // 暂时封装到了表格中，没有单独使用
@@ -50,6 +50,7 @@ Vue.prototype.$formatPercent = formatPercent
 Vue.prototype.$formatThousandth = formatThousandth
 Vue.prototype.$decimalPercentConvert = decimalPercentConvert
 Vue.prototype.$translateFirstDict = translateFirstDict
+Vue.prototype.$translateUserName = translateUserName
 
 // 重写$messgae
 Vue.prototype.$message = function(data) {
