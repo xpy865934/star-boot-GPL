db4ffaa8a52d21a180366c791778e2af90b8d26c
xpy <86934097@qq.com>
2024-05-10 11:34:32 +0800

    feat: 公司service增加mapper

--- src/main/java/com/star/starboot/system/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/star/starboot/system/service/impl/CompanyServiceImpl.java
@@ -1,10 +1,12 @@
 package com.star.starboot.system.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.star.starboot.system.entity.Company;
 import com.star.starboot.system.dao.CompanyMapper;
+import com.star.starboot.system.entity.Company;
 import com.star.starboot.system.service.CompanyService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,4 +19,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class CompanyServiceImpl extends ServiceImpl<CompanyMapper, Company> implements CompanyService {
 
+    @Resource
+    private CompanyMapper companyMapper;
 }
