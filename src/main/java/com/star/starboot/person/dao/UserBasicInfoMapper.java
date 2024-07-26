fc92dd7b7bab571eb4d59a205181a6e447db1528
xpy <86934097@qq.com>
2024-07-26 17:33:27 +0800

    feat: 用户基础信息增加queryById

--- src/main/java/com/star/starboot/person/dao/UserBasicInfoMapper.java
+++ src/main/java/com/star/starboot/person/dao/UserBasicInfoMapper.java
@@ -3,6 +3,7 @@ package com.star.starboot.person.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.star.starboot.person.entity.UserBasicInfo;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * <p>
@@ -14,4 +15,10 @@ import com.star.starboot.person.entity.UserBasicInfo;
  */
 public interface UserBasicInfoMapper extends BaseMapper<UserBasicInfo> {
 
+    /**
+     * 根据id查询
+     * @param id
+     * @return
+     */
+    UserBasicInfo queryById(@Param("id") String id);
 }
